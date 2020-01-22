a2f213d7dd74727e4fad82b0159627babda1e0ce
masoud <masoud.s.geo@gmail.com>
2020-01-23 02:07:54 +0330

    fix lint issue

---
+++ src/Provider.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Provider: React.FC = (props) => {
+  return <div>{props.children}</div>
+}
+
+export default Provider
---
+++ src/landing/Landing.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const Landing: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/Landing.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default Landing
